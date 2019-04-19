fbc85a500c2e60b1b6b5d1d6833ad325f5c742de
Rafael Hernandez Ruiz <rafassassinscreed@gmail.com>
2019-04-18 21:55:38 -0500

    me dio hueva terminar mañana

---
+++ ProyectoWAD/src/java/paquete/ActionEditarPregunta.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author Marcus
+ */
+public class ActionEditarPregunta extends ActionSupport {
+    
+    public ActionEditarPregunta() {
+    }
+    
+    public String execute() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
